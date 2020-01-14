b7b77dce72ff54f87f2cbb788e081793995df19c
lilinjun <llj96@sina.com>
2020-01-13 17:12:36 -0800

    modified path

--- include/MPC.h
+++ include/MPC.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
+// for file
+
 
 using namespace std;
 
@@ -46,5 +48,7 @@ const size_t cte_start = v_start + N;
 const size_t epsi_start = cte_start + N;
 const size_t delta_start = epsi_start + N;
 const size_t a_start = delta_start + N - 1;
+
+
 
 #endif /* MPC_H */
--- src/Controller.cpp
+++ src/Controller.cpp
@@ -16,12 +16,8 @@
 #include "Eigen-3.3/Eigen/QR"
 #include <vector>
 
-// for file
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-
+// for readcsv
+#include "utils.h"
 
 
 class Controller
@@ -36,42 +32,9 @@ class Controller
 				vector<double> path_y = {};
 				vector<double> path_goal = {};
 				Controller(){
-					int line_counter = 0;
-
-					std::fstream fin;     
-					fin.open(ros::package::getPath("rccar_ctr")+"/src/path.csv", std::ios::in); 
-					std::string line, word, temp;
-
-					while (fin >> temp) { 
-						// read an entire row and 
-						// store it in a string variable 'line' 
-						std::getline(fin, line);   
-						// used for breaking words 
-						std::stringstream s(line); 
-						// read every column data of a row and 
-						// store it in a string variable, 'word' 
-						while (getline(s, word, ',')) {  
-							// add all the column data 
-							// of a row to a vector
-							std::cout<<word<<std::endl;
-							if (line_counter==0){
-								path_x.push_back(std::stod(word)); 
-							}
-							else{
-								path_y.push_back(std::stod(word)); 
-							}
-							
-						}	
-						line_counter++;
+					if(true){
+						read_csv_path(ros::package::getPath("rccar_ros_mpc")+"/src/path.csv", path_x, path_y, path_goal);
 					}
-					std::cout<<"Read Path:"<<std::endl;
-
-					for (auto i=0; i<path_y.size(); i++){
-						std::cout<<path_x.at(i)<<","<< path_y.at(i)<<std::endl;
-					}
-
-					path_goal.push_back(path_x.back());
-					path_goal.push_back(path_y.back());
 				}
 		private:
 				MPC mpc;
